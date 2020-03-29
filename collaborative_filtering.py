3d171c50837d8e0d63d63208eaf3a615bc917512
jiyunhuang <jiyunhuang@tencent.com>
2020-03-29 14:26:02 +0800

    FEATURE: 实现 0-1 归一化

--- collaborative_filtering.py
+++ collaborative_filtering.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import sys
 
 
 class CollaborativeFiltering(object):
@@ -69,10 +70,11 @@ class CollaborativeFiltering(object):
         print("calculate user's euclidean distance finished")
         print(users_euclidean_distance_dict)
         # 计算 user 对 item 的兴趣排行
-        user_total_score_dict = dict()
+        # 统计分数的最大最小值来进行归一化
+        user_min_max_score_dict = dict()
         for user1, euclidean_distances in users_euclidean_distance_dict.items():
             self.user_cf_users_items_interest_dict[user1] = dict()
-            total_score = 0
+            min_score, max_score = sys.maxsize, -sys.maxsize
             for item, users in self.item_users_dict.items():
                 # 计算该 user 对每个 item 的兴趣值
                 score = 0
@@ -81,18 +83,17 @@ class CollaborativeFiltering(object):
                         # 只有欧氏距离与该 user 在一定范围内的 user 对这个 item 有浏览记录，才会加入计算当中
                         score += euclidean_distance
                 self.user_cf_users_items_interest_dict[user1][item] = score
-                total_score += score
-            user_total_score_dict[user1] = total_score
+                min_score, max_score = min(min_score, score), max(max_score, score)
+            user_min_max_score_dict[user1] = (min_score, max_score)
             self.user_cf_users_items_interest_dict[user1] = sorted(
                 self.user_cf_users_items_interest_dict[user1].items(),
                 key=lambda d: d[1], reverse=True)
         print("calculate user's interest in items finished")
         print(self.user_cf_users_items_interest_dict)
-        # 化成占比用于合并
-        for user, items_scores in self.user_cf_users_items_interest_dict.items():
-            for i, (item, score) in enumerate(items_scores):
-                self.user_cf_users_items_interest_dict[user][i] = (item, score / user_total_score_dict[user])
-        print("normalizatoin finished, total score:", user_total_score_dict)
+        # 归一化
+        self.user_cf_users_items_interest_dict = self.normalize(self.user_cf_users_items_interest_dict,
+                                                                user_min_max_score_dict)
+        print("normalizatoin finished, min max score dict:", user_min_max_score_dict)
         print(self.user_cf_users_items_interest_dict)
 
     def item_cf(self):
@@ -114,27 +115,27 @@ class CollaborativeFiltering(object):
         print("calculate item nearest score finished")
         print(item_nearest_score_dict)
         # 计算 user 对 item 的兴趣排行
-        user_total_score_dict = dict()
+        # 统计分数的最大最小值来进行归一化
+        user_min_max_score_dict = dict()
         for user, items in self.user_items_dict.items():
             self.item_cf_users_items_interest_dict[user] = dict()
-            total_score = 0
+            min_score, max_score = sys.maxsize, -sys.maxsize
             for item1, _ in self.item_users_dict.items():
                 score = 0
                 for item2, nearest_score in item_nearest_score_dict[item1][0:self.top_k]:
                     if item2 in items:
                         score += nearest_score
                 self.item_cf_users_items_interest_dict[user][item1] = score
-                total_score += score
-            user_total_score_dict[user] = total_score
+                min_score, max_score = min(min_score, score), max(max_score, score)
+            user_min_max_score_dict[user] = (min_score, max_score)
             self.item_cf_users_items_interest_dict[user] = sorted(self.item_cf_users_items_interest_dict[user].items(),
                                                                   key=lambda d: d[1], reverse=True)
         print("calculate user's interest in items finished")
         print(self.item_cf_users_items_interest_dict)
-        # 化成占比用于合并
-        for user, items_scores in self.item_cf_users_items_interest_dict.items():
-            for i, (item, score) in enumerate(items_scores):
-                self.item_cf_users_items_interest_dict[user][i] = (item, score / user_total_score_dict[user])
-        print("normalizatoin finished, total score:", user_total_score_dict)
+        # 归一化
+        self.item_cf_users_items_interest_dict = self.normalize(self.item_cf_users_items_interest_dict,
+                                                                user_min_max_score_dict)
+        print("normalizatoin finished, min max score dict:", user_min_max_score_dict)
         print(self.item_cf_users_items_interest_dict)
 
     def recommand(self, user_id, n):
@@ -143,3 +144,15 @@ class CollaborativeFiltering(object):
         for item, score in self.user_cf_users_items_interest_dict[user_id]:
             item_score_dict[item] = (score + item_cf_item_score_dict[item]) / 2
         return sorted(item_score_dict.items(), key=lambda d: d[1], reverse=True)[0:n]
+
+    @staticmethod
+    def normalize(users_items_interest_dict, user_min_max_score_dict):
+        """归一化"""
+        for user, items_scores in users_items_interest_dict.items():
+            min_score, max_score = user_min_max_score_dict[user]
+            for i, (item, score) in enumerate(items_scores):
+                if max_score - min_score == 0:
+                    users_items_interest_dict[user][i] = (item, 1)
+                else:
+                    users_items_interest_dict[user][i] = (item, (score - min_score) / (max_score - min_score))
+        return users_items_interest_dict
