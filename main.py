7552beb65ffb21574ffe274b5296352aa42d81b3
jiyunhuang <jiyunhuang@tencent.com>
2020-04-01 16:59:08 +0800

    FEATURE: 删除测试用 mock

--- main.py
+++ main.py
@@ -1,19 +1,8 @@
-import time
 from threading import Thread
 
 from service.collaborative_filtering import CollaborativeFiltering
-from service.recommand import recommand
-
-
-def test_recommand():
-    while True:
-        print(recommand('1', 0, 10))
-        time.sleep(0.5)
-
 
 if __name__ == '__main__':
     cf = CollaborativeFiltering()
     thread_cf = Thread(target=cf.calculate)
-    thread_recommand = Thread(target=test_recommand)
     thread_cf.start()
-    thread_recommand.start()
